d6e6b9eebc935f77a989e4567a186b5777cc9d60
st12z <ckp2004vn@gmail.com>
2025-05-03 21:27:08 +0700

    fix save discount

--- src/components/DiscountCars/DiscountCarsItem.js
+++ src/components/DiscountCars/DiscountCarsItem.js
@@ -2,17 +2,20 @@ import { Badge, Button, Card, notification, Tooltip } from "antd";
 import { useSelector } from "react-redux";
 import "./DiscountCars.scss";
 import { saveDiscountCar } from "../../service/RoomService/DiscountCarsService";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function DiscountCarsItem(props){
+function DiscountCarsItem(props) {
   const [api, contextHolder] = notification.useNotification();
-  const user = useSelector(state=>state.user);
-  const {item,myDiscounts}=props;
-  let saved=false;
-  if(myDiscounts){
-    const discount = myDiscounts.find(discount=>discount.id==item.id);
-    if(discount) saved=true;
-  }
+  const user = useSelector((state) => state.user);
+  const { item, myDiscounts } = props;
+  const [saved, setSaved] = useState(false);
+  const [loadingButton, setLoadingButton] = useState(false);
+  useEffect(() => {
+    if (myDiscounts) {
+      const discount = myDiscounts.find((discount) => discount.id == item.id);
+      if (discount) setSaved(true);
+    }
+  }, []);
   const openNotification = (placement, message, color, onClose) => {
     api.info({
       message: `Thông báo`,
@@ -25,66 +28,89 @@ function DiscountCarsItem(props){
       duration: 5,
     });
   };
-  
-  // lưu phiếu giảm giá
-  const handleSave=(id)=>{
-    if(!user){
-      openNotification("topRight","Vui lòng đăng nhập để lưu phiếu giảm giá!","red");
-    }
-    const fetchApi=async()=>{
-      const data={
-        email:user.email,
-        discountCarId:id
-      }
-      const res= await saveDiscountCar(data);
-      console.log(res);
-      if(res.code==200){
-        openNotification("topRight","Lưu phiếu giảm giá thành công!","green");
-      }
-      else if(res.code==404){
-        openNotification("topRight","Phiếu giảm giá này đã được lưu.Vui lòng kiểm tra trong mục phiếu giảm giá!","red");
-      }
-      else{
-        openNotification("topRight","Phiếu giảm giá đã hết số lượng!","red");
-      }
-    }
-    fetchApi();
-  }
 
-  return(
+  // lưu phiếu giảm giá
+  const handleSave = (id) => {
+    if (!user) {
+      openNotification(
+        "topRight",
+        "Vui lòng đăng nhập để lưu phiếu giảm giá!",
+        "red"
+      );
+    }
+    const fetchApi = async () => {
+      const data = {
+        email: user.email,
+        discountCarId: id,
+      };
+      const res = await saveDiscountCar(data);
+      console.log(res);
+      if (res.code == 200) {
+        openNotification("topRight", "Lưu phiếu giảm giá thành công!", "green");
+      } else if (res.code == 404) {
+        openNotification(
+          "topRight",
+          "Phiếu giảm giá này đã được lưu.Vui lòng kiểm tra trong mục phiếu giảm giá!",
+          "red"
+        );
+      } else {
+        openNotification("topRight", "Phiếu giảm giá đã hết số lượng!", "red");
+      }
+    };
+    setLoadingButton(true);
+    setTimeout(() => {
+      fetchApi();
+      setSaved(true);
+      setLoadingButton(false);
+    }, 3000);
+  };
+
+  return (
     <>
       {contextHolder}
-      <Badge.Ribbon 
-      text={`${item.discountValue}%`} 
-      color="red" 
-      className="discount__item__badge">
-      <div className="discount__item">
-        <div className="discount__item__img">
-          <img src={item.images} alt="" />
-        </div>
-        <div className="discount__item__content">
-          <h3>{item.code}</h3>
-          <p><b>Thời gian áp dụng:</b> {new Date(item.startDate).toLocaleDateString()} - {new Date(item.endDate).toLocaleDateString()}</p>
-        </div>
-        <div className="discount__item__save">
-          {saved ? (
-            <Tooltip title="Đã có">
-              <Button type="primary" onClick={()=>handleSave(item.id)} disabled={saved}>
+      <Badge.Ribbon
+        text={`${item.discountValue}%`}
+        color="red"
+        className="discount__item__badge"
+      >
+        <div className="discount__item">
+          <div className="discount__item__img">
+            <img src={item.images} alt="" />
+          </div>
+          <div className="discount__item__content">
+            <h3>{item.code}</h3>
+            <p>
+              <b>Thời gian áp dụng:</b>{" "}
+              {new Date(item.startDate).toLocaleDateString()} -{" "}
+              {new Date(item.endDate).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="discount__item__save">
+            {saved ? (
+              <Tooltip title="Đã có">
+                <Button
+                  type="primary"
+                  onClick={() => handleSave(item.id)}
+                  disabled={saved}
+                  loading={loadingButton}
+                >
+                  Lưu
+                </Button>
+              </Tooltip>
+            ) : (
+              <Button
+                type="primary"
+                onClick={() => handleSave(item.id)}
+                disabled={saved}
+                loading={loadingButton}
+              >
                 Lưu
-            </Button>
-            </Tooltip>
-          ):(
-            <Button type="primary" onClick={()=>handleSave(item.id)} disabled={saved}>
-              Lưu
-            </Button>
-          )}
-          
+              </Button>
+            )}
+          </div>
         </div>
-      </div>
-    </Badge.Ribbon>
+      </Badge.Ribbon>
     </>
-    
-    
-  )
+  );
 }
 export default DiscountCarsItem;
--- src/components/Discounts/DiscountItem.js
+++ src/components/Discounts/DiscountItem.js
@@ -1,16 +1,20 @@
 import { Badge, Button, Card, notification, Tooltip } from "antd";
 import { useSelector } from "react-redux";
 import { saveDiscount } from "../../service/RoomService/DiscountService";
+import { useEffect, useState } from "react";
 
-function DiscountItem(props){
+function DiscountItem(props) {
   const [api, contextHolder] = notification.useNotification();
-  const user = useSelector(state=>state.user);
-  const {item,myDiscounts}=props;
-  let saved=false;
-  if(myDiscounts){
-    const discount = myDiscounts.find(discount=>discount.id==item.id);
-    if(discount) saved=true;
-  }
+  const user = useSelector((state) => state.user);
+  const { item, myDiscounts } = props;
+  const [loadingButton, setLoadingButton] = useState(false);
+  const [saved, setSaved] = useState(false);
+  useEffect(() => {
+    if (myDiscounts) {
+      const discount = myDiscounts.find((discount) => discount.id == item.id);
+      if (discount) setSaved(true);
+    }
+  }, []);
   const openNotification = (placement, message, color, onClose) => {
     api.info({
       message: `Thông báo`,
@@ -23,71 +27,110 @@ function DiscountItem(props){
       duration: 5,
     });
   };
-  
-  // lưu phiếu giảm giá
-  const handleSave=(id)=>{
-    if(!user){
-      openNotification("topRight","Vui lòng đăng nhập để lưu phiếu giảm giá!","red");
-    }
-    const fetchApi=async()=>{
-      const data={
-        email:user.email,
-        discountId:id
-      }
-      const res= await saveDiscount(data);
-      console.log(res);
-      if(res.code==200){
-        openNotification("topRight","Lưu phiếu giảm giá thành công!","green");
-      }
-      else if(res.code==404){
-        openNotification("topRight","Phiếu giảm giá này đã được lưu.Vui lòng kiểm tra trong mục phiếu giảm giá!","red");
-      }
-      else{
-        openNotification("topRight","Phiếu giảm giá đã hết số lượng!","red");
-      }
-    }
-    fetchApi();
-  }
 
-  return(
+  // lưu phiếu giảm giá
+  const handleSave = (id) => {
+    if (!user) {
+      openNotification(
+        "topRight",
+        "Vui lòng đăng nhập để lưu phiếu giảm giá!",
+        "red"
+      );
+    }
+    const fetchApi = async () => {
+      const data = {
+        email: user.email,
+        discountId: id,
+      };
+      try {
+        const res = await saveDiscount(data);
+        console.log(res);
+        if (res.code == 200) {
+          openNotification(
+            "topRight",
+            "Lưu phiếu giảm giá thành công!",
+            "green"
+          );
+        } else if (res.code == 404) {
+          openNotification(
+            "topRight",
+            "Phiếu giảm giá này đã được lưu.Vui lòng kiểm tra trong mục phiếu giảm giá!",
+            "red"
+          );
+        } else {
+          openNotification(
+            "topRight",
+            "Phiếu giảm giá đã hết số lượng!",
+            "red"
+          );
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    setLoadingButton(true);
+    setTimeout(() => {
+      fetchApi();
+      setSaved(true);
+      setLoadingButton(false);
+    }, 3000);
+  };
+
+  return (
     <>
       {contextHolder}
-      <Badge.Ribbon 
-      text={`${item.discountType === "PERCENT" 
-        ? `${item.discountValue} %` 
-        : `${new Intl.NumberFormat('vi-VN').format(item.discountValue)} VNĐ`
-      }`} 
-      color="red" 
-      className="discount__item__badge">
-      <div className="discount__item">
-        <div className="discount__item__img">
-          <img src={item.image} alt="" />
-        </div>
-        <div className="discount__item__content">
-          <h3>{item.code}</h3>
-          
-          <p><b>Giá tối thiểu:</b> {new Intl.NumberFormat('vi-VN').format(item.minBookingAmount)} VNĐ</p>
-          <p><b>Thời gian áp dụng:</b> {new Date(item.startDate).toLocaleDateString()} - {new Date(item.endDate).toLocaleDateString()}</p>
-        </div>
-        <div className="discount__item__save">
-          {saved ? (
-            <Tooltip title="Đã có">
-              <Button type="primary" onClick={()=>handleSave(item.id)} disabled={saved}>
+      <Badge.Ribbon
+        text={`${
+          item.discountType === "PERCENT"
+            ? `${item.discountValue} %`
+            : `${new Intl.NumberFormat("vi-VN").format(item.discountValue)} VNĐ`
+        }`}
+        color="red"
+        className="discount__item__badge"
+      >
+        <div className="discount__item">
+          <div className="discount__item__img">
+            <img src={item.image} alt="" />
+          </div>
+          <div className="discount__item__content">
+            <h3>{item.code}</h3>
+
+            <p>
+              <b>Giá tối thiểu:</b>{" "}
+              {new Intl.NumberFormat("vi-VN").format(item.minBookingAmount)} VNĐ
+            </p>
+            <p>
+              <b>Thời gian áp dụng:</b>{" "}
+              {new Date(item.startDate).toLocaleDateString()} -{" "}
+              {new Date(item.endDate).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="discount__item__save">
+            {saved ? (
+              <Tooltip title="Đã có">
+                <Button
+                  type="primary"
+                  onClick={() => handleSave(item.id)}
+                  disabled={saved}
+                  loading={loadingButton}
+                >
+                  Lưu
+                </Button>
+              </Tooltip>
+            ) : (
+              <Button
+                type="primary"
+                onClick={() => handleSave(item.id)}
+                disabled={saved}
+                loading={loadingButton}
+              >
                 Lưu
-            </Button>
-            </Tooltip>
-          ):(
-            <Button type="primary" onClick={()=>handleSave(item.id)} disabled={saved}>
-              Lưu
-            </Button>
-          )}
-          
+              </Button>
+            )}
+          </div>
         </div>
-      </div>
-    </Badge.Ribbon>
+      </Badge.Ribbon>
     </>
-    
-    
-  )
+  );
 }
 export default DiscountItem;
