099a0f573e8a8506ca3352435e06daf8c600ddd8
st12z <ckp2004vn@gmail.com>
2025-07-23 18:07:22 +0700

    config variable

--- src/utils/dist/variable.dev.js
+++ src/utils/dist/variable.dev.js
@@ -4,17 +4,17 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.API_DOMAIN_MESSAGES = exports.API_DOMAIN_PAYMENTS = exports.API_DOMAIN_ROOMS = exports.API_DOMAIN_BOOKINGS = exports.API_DOMAIN_USERS = exports.API_DOMAIN = exports.API_DOMAIN_SOCKET = void 0;
-var API_DOMAIN_SOCKET = "http://localhost:8080";
+var API_DOMAIN_SOCKET = "https://rooms-s1.onrender.com";
 exports.API_DOMAIN_SOCKET = API_DOMAIN_SOCKET;
-var API_DOMAIN = "http://localhost:8072/bookinghotel";
+var API_DOMAIN = "https://gatewayserver-s1.onrender.com/bookinghotel";
 exports.API_DOMAIN = API_DOMAIN;
-var API_DOMAIN_USERS = "http://localhost:8072/bookinghotel/users/api/users";
+var API_DOMAIN_USERS = "https://gatewayserver-s1.onrender.com/bookinghotel/users/api/users";
 exports.API_DOMAIN_USERS = API_DOMAIN_USERS;
-var API_DOMAIN_BOOKINGS = "http://localhost:8072/bookinghotel/bookings/api";
+var API_DOMAIN_BOOKINGS = "https://gatewayserver-s1.onrender.com/bookinghotel/bookings/api";
 exports.API_DOMAIN_BOOKINGS = API_DOMAIN_BOOKINGS;
-var API_DOMAIN_ROOMS = "http://localhost:8072/bookinghotel/rooms/api";
+var API_DOMAIN_ROOMS = "https://gatewayserver-s1.onrender.com/bookinghotel/rooms/api";
 exports.API_DOMAIN_ROOMS = API_DOMAIN_ROOMS;
-var API_DOMAIN_PAYMENTS = "http://localhost:8072/bookinghotel/payments/api";
+var API_DOMAIN_PAYMENTS = "https://gatewayserver-s1.onrender.com/bookinghotel/payments/api";
 exports.API_DOMAIN_PAYMENTS = API_DOMAIN_PAYMENTS;
-var API_DOMAIN_MESSAGES = "http://localhost:8072/bookinghotel/messages/api";
+var API_DOMAIN_MESSAGES = "https://gatewayserver-s1.onrender.com/bookinghotel/messages/api";
 exports.API_DOMAIN_MESSAGES = API_DOMAIN_MESSAGES;
--- src/utils/variable.js
+++ src/utils/variable.js
@@ -1,8 +1,8 @@
-export const API_DOMAIN_SOCKET = "http://localhost:8080";
-export const API_DOMAIN = "http://localhost:8072/bookinghotel";
-export const API_DOMAIN_USERS="http://localhost:8072/bookinghotel/users/api/users";
-export const API_DOMAIN_BOOKINGS="http://localhost:8072/bookinghotel/bookings/api";
-export const API_DOMAIN_ROOMS="http://localhost:8072/bookinghotel/rooms/api";
-export const API_DOMAIN_PAYMENTS="http://localhost:8072/bookinghotel/payments/api";
-export const API_DOMAIN_MESSAGES="http://localhost:8072/bookinghotel/messages/api";
+export const API_DOMAIN_SOCKET = "https://rooms-s1.onrender.com";
+export const API_DOMAIN = "https://gatewayserver-s1.onrender.com/bookinghotel";
+export const API_DOMAIN_USERS="https://gatewayserver-s1.onrender.com/bookinghotel/users/api/users";
+export const API_DOMAIN_BOOKINGS="https://gatewayserver-s1.onrender.com/bookinghotel/bookings/api";
+export const API_DOMAIN_ROOMS="https://gatewayserver-s1.onrender.com/bookinghotel/rooms/api";
+export const API_DOMAIN_PAYMENTS="https://gatewayserver-s1.onrender.com/bookinghotel/payments/api";
+export const API_DOMAIN_MESSAGES="https://gatewayserver-s1.onrender.com/bookinghotel/messages/api";
 
